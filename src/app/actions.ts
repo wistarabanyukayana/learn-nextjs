7ee7aaee33884ce78d0cca83aa8750e95bcc861b
Wistara Banyu Kayana <wisthara.banyu.kayana@gmail.com>
2025-06-17 21:09:54 +0700

    finished cache

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,6 +3,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { prisma } from "./utils/db";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export async function handleSubmission(formData: FormData) {
   const { getUser } = getKindeServerSession();
@@ -26,6 +27,8 @@ export async function handleSubmission(formData: FormData) {
       authorName: user?.given_name as string,
     },
   });
+
+  revalidatePath("/");
 
   return redirect("/dashboard");
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { prisma } from "./utils/db";
 import { Suspense } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 
+export const revalidate = 60;
+
 async function getData() {
   await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate loading delay
   const data = await prisma.blogPost.findMany({
