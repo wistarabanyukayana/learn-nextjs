b00376a10c9928b96a5b51a32ad2fd009805499e
Wistara Banyu Kayana <wisthara.banyu.kayana@gmail.com>
2025-06-17 18:49:10 +0700

    Security Optimization

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -8,11 +8,15 @@ export async function handleSubmission(formData: FormData) {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
+  if (!user) {
+    return redirect("/api/auth/register");
+  }
+
   const title = formData.get("title");
   const content = formData.get("content");
   const url = formData.get("url");
 
-  const data = await prisma.blogPost.create({
+  await prisma.blogPost.create({
     data: {
       title: title as string,
       content: content as string,
