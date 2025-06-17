74c69173c501f9822e91e1c08070dd3efa112535
Wistara Banyu Kayana <wisthara.banyu.kayana@gmail.com>
2025-06-17 21:05:34 +0700

    getting session from client

--- src/components/general/Navbar.tsx
+++ src/components/general/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
 import {
@@ -5,11 +7,11 @@ import {
   LoginLink,
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
-export async function Navbar() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+export function Navbar() {
+  const { getUser } = useKindeBrowserClient();
+  const user = getUser();
 
   return (
     <nav className="py-5 flex items-center justify-between">
